c7a1ea3a5dcba9ff4130623988bf598f670e5b54
gibin-rajan <98406949+gibin-rajan@users.noreply.github.com>
2024-03-20 20:11:00 +0530

    Adding Leetcode 75 solution

---
+++ LeetCode 75/392-Is Subsequence.py
@@ -0,0 +1,14 @@
+class Solution:
+  def isSubsequence(self, s, t):
+    if not s:
+      return True
+
+    i = 0
+    for c in t:
+      if s[i] == c:
+        i += 1
+        if i == len(s):
+          return True
+
+    return False
+        
