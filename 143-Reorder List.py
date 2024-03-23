7433fdb7cd56c32f6f72e5af76a9b198e2b30276
gibin-rajan <98406949+gibin-rajan@users.noreply.github.com>
2024-03-23 19:10:33 +0530

    Adding Leetcode solution

---
+++ 143-Reorder List.py
@@ -0,0 +1,40 @@
+class Solution:
+  def reorderList(self, head):
+    def findMid(head):
+      prev = None
+      slow = head
+      fast = head
+
+      while fast and fast.next:
+        prev = slow
+        slow = slow.next
+        fast = fast.next.next
+      prev.next = None
+
+      return slow
+
+    def reverse(head):
+      prev = None
+      curr = head
+
+      while curr:
+        next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+
+      return prev
+
+    def merge(l1, l2):
+      while l2:
+        next = l1.next
+        l1.next = l2
+        l1 = l2
+        l2 = next
+
+    if not head or not head.next:
+      return
+
+    mid = findMid(head)
+    reversed = reverse(mid)
+    merge(head, reversed)
