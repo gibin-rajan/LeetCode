4510ac0748f7a7fd116a066a61021bc797fe5a27
gibin-rajan <98406949+gibin-rajan@users.noreply.github.com>
2024-04-04 21:15:55 +0530

    Adding Leetcode 75 solution

---
+++ LeetCode 75/394-Decode String.py
@@ -0,0 +1,22 @@
+class Solution:
+  def decodeString(self, s):
+    stack = []  # (prevStr, repeatCount)
+    currStr = ''
+    currNum = 0
+
+    for c in s:
+      if c.isdigit():
+        currNum = currNum * 10 + int(c)
+      else:
+        if c == '[':
+          stack.append((currStr, currNum))
+          currStr = ''
+          currNum = 0
+        elif c == ']':
+          prevStr, num = stack.pop()
+          currStr = prevStr + num * currStr
+        else:
+          currStr += c
+
+    return currStr
+        
