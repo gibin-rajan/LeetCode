7cd0e746438f6c60c40b90de679e17ff878dd20f
gibin-rajan <98406949+gibin-rajan@users.noreply.github.com>
2024-04-18 19:34:15 +0530

    Adding Leetcode solution

---
+++ LeetCode 75/700-Search in a Binary Search Tree.py
@@ -0,0 +1,7 @@
+class Solution:
+    def searchBST(self, root, val):
+        if root is None:
+            return None
+        if root.val== val:
+            return root
+        return self.searchBST(root.left, val) if val< root.val else self.searchBST(root.right, val)
