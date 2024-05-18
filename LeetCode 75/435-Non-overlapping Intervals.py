75d5fac8780167e6708ed6f1771117f0408f34b0
gibin-rajan <98406949+gibin-rajan@users.noreply.github.com>
2024-05-18 17:45:56 +0530

    Adding Leetcode 75 solution

---
+++ LeetCode 75/435-Non-overlapping Intervals.py
@@ -0,0 +1,10 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals):
+        intervals.sort(key=lambda interval: interval[1])
+        end = intervals[0][1]
+        count = len(intervals) - 1
+        for i in range(1, len(intervals)):
+            if intervals[i][0] >= end:
+                end = intervals[i][1]
+                count -= 1
+        return count
