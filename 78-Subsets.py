29fc8ad5d5a9c3309d9bb134801dcc25dba3edd8
gibin-rajan <98406949+gibin-rajan@users.noreply.github.com>
2024-05-21 20:38:13 +0530

    Adding Leetcode solution

---
+++ 78-Subsets.py
@@ -0,0 +1,10 @@
+class Solution:
+    def subsets(self, nums):
+        def backtrack(start, path):
+            result.append(path)
+            for i in range(start, len(nums)):
+                backtrack(i + 1, path + [nums[i]])
+
+        result = []
+        backtrack(0, [])
+        return result
