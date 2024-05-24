fb0ccbf2f9e98fc0664a9defbf3cd6bd866aebbc
gibin-rajan <98406949+gibin-rajan@users.noreply.github.com>
2024-05-24 19:11:19 +0530

    Adding Leetcode solution

--- 1255-Maximum Score Words Formed by Letters.py
+++ 1255-Maximum Score Words Formed by Letters.py
@@ -1,30 +1,41 @@
 class Solution:
     def maxScoreWords(self, words, letters, score):
-        lettersCounter = Counter(letters)
-        totalScore = 0
+        n = len(words)
+        res = 0
 
-        def explore(index, letterCounter, currScore):
-            nonlocal totalScore
+        # count letters
+        letters_count = [0 for _ in range(26)]
+        for letter in letters:
+            ind = ord(letter) - 97
+            letters_count[ind] += 1
+        
+        # calculate words
+        words_scores = {}
+        for word in words:
+            s = 0
+            for char in word:
+                ind = ord(char) - 97
+                s += score[ind]
+            words_scores[word] = s
+ 
+        for mask in range(0, 2 ** n, 1): # There will be exactly 2^n different states
+            cur_count = [0 for _ in range(26)]
+            can_create = True
+            cur_score = 0
 
-            totalScore = max(totalScore, currScore)
-            if index == len(words):
-                return
+            for word_ind in range(n):
+                if (mask & (1 << word_ind)) != 0: # if in mask bit of this word is 1
+                    word = words[word_ind]
+                    cur_score += words_scores[word]
+                    for char in word:
+                        ind = ord(char) - 97
+                        cur_count[ind] += 1
+                        if cur_count[ind] > letters_count[ind]:
+                            can_create = False
+                            break
+                if not can_create:
+                    break
+            if can_create and cur_score > res: 
+                res = cur_score
 
-            for i in range(index, len(words)):
-                tmpCounter = copy.deepcopy(letterCounter)
-                word = words[i]
-                wordScore = 0
-                isValid = True
-
-                for ch in word:
-                    if ch in tmpCounter and tmpCounter[ch] > 0:
-                        tmpCounter[ch] -= 1
-                        wordScore += score[ord(ch) - ord("a")]
-                    else:
-                        isValid = False
-                        break
-                if isValid:
-                    explore(i + 1, tmpCounter, currScore + wordScore)
-
-        explore(0, lettersCounter, 0)
-        return totalScore
+        return res
